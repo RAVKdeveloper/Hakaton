67b440e18a04ded920a69dbce6834cfbbb79665d
unknown <shcherbakovkirill2@gmail.com>
2024-05-04 15:14:42 +0300

    fead: add docker compose && init mongoose

--- server/app/app.ts
+++ server/app/app.ts
@@ -1,20 +1,23 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
 const app = express()
 const port = Number(process.env.PORT) ?? 3005
+const dbUrl = process.env.DB_URL ?? ''
 
 app.use(cors({ origin: process.env.CLIENT_URL }))
 app.use(express.json())
 
-function bootstrap() {
+async function bootstrap() {
   try {
+    await mongoose.connect(dbUrl)
     app.listen(port, () => console.log(`Server listen ${port}`))
   } catch (e) {
-    console.log(`Initial erro: ${e}`)
+    console.log(`Initial error: ${e}`)
   }
 }
 
